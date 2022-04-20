3a5666220f595bfd0d8a7db522e0775844c1446b
chani017 <sfgdy058@gmail.com>
2022-04-20 23:41:09 +0900

    light change

--- index.js
+++ index.js
@@ -24,20 +24,9 @@ import { DRACOLoader } from './jsm/loaders/DRACOLoader.js';
 
         camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 20 );
 
-        const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444 );
-        hemiLight.position.set( 0, 20, 0 );
-        scene.add( hemiLight );
-
-        const dirLight = new THREE.DirectionalLight( 0xffffff );
-        dirLight.position.set( - 3, 10, - 10 );
-        dirLight.castShadow = true;
-        dirLight.shadow.camera.top = 2;
-        dirLight.shadow.camera.bottom = - 2;
-        dirLight.shadow.camera.left = - 2;
-        dirLight.shadow.camera.right = 2;
-        dirLight.shadow.camera.near = 0.1;
-        dirLight.shadow.camera.far = 40;
-        scene.add( dirLight );
+        light = new THREE.DirectionalLight(0xffffff, 0.5);
+        light.castShadow = true;
+        scene.add(light);
 
         //
 
@@ -71,12 +60,6 @@ import { DRACOLoader } from './jsm/loaders/DRACOLoader.js';
                 model.scale.set(0.3, 0.3, 0.3);
                 scene.add(model);
             }
-
-        const dracoLoader = new DRACOLoader()
-
-        dracoLoader.setDecoderPath('./draco/')
-        gltfLoader.setDRACOLoader(dracoLoader)       
-
         }
 
         controller = renderer.xr.getController( 0 );
