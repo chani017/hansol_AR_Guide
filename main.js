3f6c25e0c6eb8b6e16eaddc76ba50f2798d509b0
chani017 <sfgdy058@gmail.com>
2022-04-23 04:47:39 +0900

    test

--- main.js
+++ main.js
@@ -87,7 +87,7 @@ import { GLTFLoader } from './jsm/loaders/GLTFLoader.js';
 
                 reticle.matrix.decompose( model.position, model.quaternion, model.scale );
                 model.rotation.y = 5;
-                model.scale.set(0.05, 0.05, 0.05);
+                model.scale.set(0.02, 0.02, 0.02);
                 model.receiveShadow = true;
                 model.add( Audio );
                 scene.add(model);
